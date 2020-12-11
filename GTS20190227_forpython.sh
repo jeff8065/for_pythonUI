c5e31b8b776e3171955a07684b6017dba9334513
jeff8065 <37102860+jeff8065@users.noreply.github.com>
2020-12-11 17:26:13 +0800

    Update GTS20190227_forpython.sh

--- GTS20190227_forpython.sh
+++ GTS20190227_forpython.sh
@@ -220,7 +220,7 @@ function ChooseVersion(){
 		echo Folder  :   "/3pl_report"/gts/8.0r2/$brand/$name/$brand/$name
 		compareFingerPrint
 		#echo "/CTS_tool/GTS_tool/8.0_r1 Preview/android-gts-8-R1\(O-Q\)-6720564.zip"
-		unzip -o -q /CTS_tool/GTS_tool/"8.0_r2 Formal"/"android-gts-8-R1(O-Q)-6720564.zip" -d "/3pl_report"/gts/8.0r2/$brand/$name
+		unzip -o -q /CTS_tool/GTS_tool/"8.0_r2 Formal"/"android-gts-8-R2(8-10)--6955212.zip" -d "/3pl_report"/gts/8.0r2/$brand/$name
 		echo $password | sudo -S update-java-alternatives --set java-1.11.0-openjdk-amd64
 		#adb -s $serialID shell dumpsys appwidget >> /GTS_tool/3PL/5.1r2/$brand/$name/android-gts/tools/"$name"widget
 		#adb $serial_options shell am start -a android.intent.action.CALL -d tel:6767
