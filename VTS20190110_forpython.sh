106fef1a812288e1ff0680fa019649cc098a2073
jeff8065 <37102860+jeff8065@users.noreply.github.com>
2021-02-01 15:42:44 +0800

    20210201

--- VTS20190110_forpython.sh
+++ VTS20190110_forpython.sh
@@ -271,7 +271,7 @@ function CTSDIRECTORY_python(){
 	androidVersion=$(adb -s ${serialArray[$1]} shell getprop | grep "\[ro.build.version.release\]" | sed 's/\[ro.build.version.release\]: \[//' | sed 's/8.0.*/8.0/' | sed 's/8.1.*/8.1/' | sed 's/9.0.*/9.0/' | sed 's/10.0.*/10/' | sed 's/11.0.*/11/'  )
 	if [ ${androidVersion:0:1} == "9" ];then
 		androidVersion="9.0"
-	elif [ ${androidVersion:0:1} == "11]" ];then
+	elif [ ${androidVersion:0:1} == '11]' ];then
 		androidVersion="11"
 	fi
 #	echo "This device android verision is" $androidVersion
@@ -388,7 +388,7 @@ function ChooseGsiaospVersion(){
 #	echo 'CPU:'$cpuType
 #        echo toolzip $toolzip
 
-	if [ "$androidVersion" = "8.0" ]|| [ "$androidVersion" = "11"  ]|| [ "$androidVersion" = "11]"  ];then
+	if [ "$androidVersion" = "8.0" ]|| [ "$androidVersion" = "11"  ];then
 		mkdir -p /3pl_report/aosp/"${version[1]}"/$brand/$name
 		echo Folder  :   /3pl_report/aosp/"${version[1]}"/$brand/$name
 		
